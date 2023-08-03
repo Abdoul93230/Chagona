a26fb84fb032ab1128802469bfd5d2f097bdc944
Abdoul_Razak <abdoulrazak9323@gmail.com>
2023-08-03 08:09:38 -0700

    updated

--- src/components/ProductDet/ProductDet.js
+++ src/components/ProductDet/ProductDet.js
@@ -549,6 +549,7 @@ function ProductDet({ product }) {
   return (
     <div className="ProductDet">
       <Helmet>
+        <title>{VP?.name}</title>
         <meta property="og:title" content={VP?.name} />
         <meta property="og:description" content={VP?.description} />
         <meta property="og:image" content={VP?.image1} />
